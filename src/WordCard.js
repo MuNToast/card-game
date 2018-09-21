2b8d812349eb2352ef5a3c60e672a01d89f02a68
MuNToast <32356432+MuNToast@users.noreply.github.com>
2018-09-22 00:05:02 +0700

    ตอนที่่ 6 จบแล้ว

--- src/WordCard.js
+++ src/WordCard.js
@@ -14,44 +14,34 @@ const prepareStateFromWord = (given_word) => {
     }
 }
 export default class WordCard extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = prepareStateFromWord(this.props.value)
     }
     activationHandler = (c) => {
-        let guess = [this.state.guess]+c
-        this.setState({guess})
-        if(guess.length === this.state.chars.length){
-            console.log(this.state.word )
-            if(guess === this.state.word){
-                this.setState({guess: [], completed: true})
+        let guess = [this.state.guess] + c
+        this.setState({ guess })
+        if (guess.length === this.state.chars.length) {
+            console.log(this.state.word)
+            if (guess === this.state.word) {
+                this.setState({ guess: [], completed: true })
             }
-            else{
-                this.setState({guess: [], attempt: this.state.attempt + 1})
+            else {
+                this.setState({ guess: [], attempt: this.state.attempt + 1 })
             }
         }
     }
 
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="App">
                 {
                     Array.from(this.state.chars).map(
-                        (c, i) => <CharacterCard value = {c} key = {i} attempt={this.state.attempt}
-                        activationHandler = {this.activationHandler}/>
+                        (c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt}
+                            activationHandler={this.activationHandler} />
                     )
-                }
-                <div className="result">
-                {console.log(this.state.completed)}
-                <p>Round : {this.state.attempt}</p>
-                <p>{this.state.completed && this.state.attempt ==  1 ? "so clever, you can solved in first round." : ""}</p>
-                <p>{this.state.completed && this.state.attempt >=  4 ? "good job but you should practice more." : ""}</p>
-                <p>{this.state.completed == false && this.state.attempt >=  10 ? "you lose!!!!" : ""}</p>
-                <p>{this.state.completed == false && this.state.attempt >=  10 ? this.state.attempt = 1 : ""}</p>
-                <p>{this.state.completed ? alert('YOU ARE THE WINNER!') : ""}</p>
-                </div>
-            </div>
+                }            </div>
         )
     }
 }
