9861e37c2185f1e820f6969dcc35178a381e4259
MuNToast <32356432+MuNToast@users.noreply.github.com>
2018-09-22 00:57:30 +0700

    Change Word

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "EntamoebaHistolytica";
+const word = "Bonjour";
 class App extends Component {
   render() {
     return (
